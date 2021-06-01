af6a2591539ffa8ce88f258aac5230fd4acfdb9f
N8Brown <nathan.a.brown@outlook.com>
2021-06-01 15:32:06 -0400

    Code updated to reflect course progress through section 25

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+# from django.http import HttpResponseRedirect
+from .models import List
+from .forms import ListForm
+
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        else:
+            messages.warning(request, ('An error prevented the item from being added to the list. Please try again'))
+
+        messages.success(request, ('Item has been successfully added to the list'))
+        list = List.objects.all()
+        context = {
+            'list': list,
+        }
+        return render(request, 'home.html', context)
+
+    else:
+        list = List.objects.all()
+        context = {
+            'list': list,
+        }
+        return render(request, 'home.html', context)
 
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Item has been successfully deleted from the list'))
+    return redirect('home')
